f5aa4b904e93b79e6dff9ede0d613d7ee3a5be96
Alex <alexbaltaga@gmail.com>
2023-05-24 19:34:23 +0300

    Created constructor and print

--- Card.cpp
+++ Card.cpp
@@ -1,1 +1,34 @@
 #include "Card.h"
+
+Card::Card(CardType type, const CardStats& stats)
+{
+    m_effect = type;
+    m_stats = stats;
+}
+
+void Card::applyEncounter(Player& player) const
+{
+    //do encounter
+
+    /* win true if player won
+    printBattleResult(win);
+    */
+}
+
+void Card::printInfo() const
+{
+    switch(m_effect)
+    {
+        case Battle:
+        printBattleCardInfo(*this);
+        return;
+        case Buff:
+        printBuffCardInfo(*this);
+        return;
+        case Heal:
+        printHealCardInfo(*this);
+        return;
+        case Treasure:
+        printTreasureCardInfo(*this);
+    }
+}
